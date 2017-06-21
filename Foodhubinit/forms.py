1c76a54a45d5ed6733778e989589f55755c87f46
rbadri91 <rbadri01@gmail.com>
2017-06-21 05:25:10 -0400

    Listing restaurants and adding menu pages

--- Foodhubinit/forms.py
+++ Foodhubinit/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
@@ -13,3 +13,20 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+class BootstrapPaginator(Paginator):
+    def __init__(self, *args, **kwargs):
+        """        
+        :param wing_pages: How many pages will be shown before and after current page.
+        """
+        self.wing_pages = kwargs.pop('wing_pages', 3)
+        super(BootstrapPaginator, self).__init__(*args, **kwargs)
+
+    def _get_page(self, *args, **kwargs):
+        self.page = super(BootstrapPaginator, self)._get_page(*args, **kwargs)
+        return self.page
+
+    @property
+    def page_range(self):
+        return range(max(self.page.number - self.wing_pages, 1),
+                     min(self.page.number + self.wing_pages + 1, self.num_pages + 1))
--- Foodhubinit/views.py
+++ Foodhubinit/views.py
@@ -7,6 +7,12 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode,  urlsafe_base64_decode
 from django.template.loader import render_to_string
+import json
+from bson import json_util
+from bson.json_util import dumps
+import urllib.request,urllib.parse
+import requests
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from Foodhubinit.forms import SignUpForm
 from Foodhubinit.tokens import account_activation_token
@@ -19,10 +25,11 @@ def index(request):
 	json_data = open('Foodhubinit/static/Foodhub/json/us_state_capitals.json')
 	data1 = json.load(json_data)
 	capitals =list()
+	request.session['cities'] = {}
+	request.session.modified = True
 	
 	for attribute, value in data1.items():
 		capitals.append(value['capital'])
-	print("capitals:",capitals)	
 	return render(request, 'Foodhubinit/index.html',{"capitals":capitals})
 
 @login_required
@@ -81,11 +88,163 @@ def listStates(request):
 		states.append(value)
 	return render(request, 'Foodhubinit/listAllStates.html',{"states":states})
 
-def cities(request,state):
-	pass
+def cities(request,state_name):
+	json_data = open('Foodhubinit/static/Foodhub/json/cities.json')
+	data = json.load(json_data)
+	cities = list();
+	for cityObj in data:
+		if cityObj['state'] == state_name:
+			cities.append(cityObj['city'])
+
+	print("citiesNew:",cities)
+	# json_data = open('Foodhubinit/static/Foodhub/json/counties.json')
+	# data1 = json.load(json_data)
+	# cities = data1.get(state_name)
+	request.session["cities"] = cities
+	return render(request, 'Foodhubinit/listAllCities.html',{"cities":cities})
+
+def city(request,city_name):
+	json_data = open('Foodhubinit/static/Foodhub/json/cities.json')
+	data1 = json.load(json_data)
+	nearbyCities = list()
+	ratings =dict();
+	print("city_name:",city_name)
+	state = ""
+	if request.session["cities"]:
+		nearbyCities = request.session["cities"]
+	else:
+		for cityObj in data1:
+			if cityObj['city'] == city_name:
+				state = cityObj['state']
+				break
+		print("state here:",state);		
+		for cityObj2 in data1:
+			if cityObj2['state'] == state:
+					nearbyCities.append(cityObj2['city'])
+
+	print("nearbyCities:",nearbyCities)
+	nearbyCities= sorted(nearbyCities,key=str.lower);
+	pageNum = request.GET.get('pageNum')
+	if city_name in nearbyCities:
+		nearbyCities.remove(city_name)
+	# if(not("city" in request.session) or (pageNum == 1 and request.session["city"] != city_name)):
+	
+	restaurants = getRestaurants("delivery",city_name)
+	print("restaurants:",restaurants)
+	request.session["restaurants"] = restaurants;
+	# else:
+		# restaurants = request.session["restaurants"]
+	request.session["city"] = city_name	
+	totalRestaurants = len(restaurants)
+	print("totalRestaurants:",totalRestaurants)
+	print("pageNum:",pageNum)
+	paginator = Paginator(restaurants, 20)
+	try:
+		displayRestaurants = paginator.page(pageNum)
+	except PageNotAnInteger:
+		displayRestaurants = paginator.page(1)
+	except EmptyPage:
+		displayRestaurants = paginator.page(paginator.num_pages)
+	# displayRestaurants = restaurants[(pageNum-1)* 20 : ((pageNum-1)* 20 +20 )]
+	print("displayRestaurantshere:",displayRestaurants)
+	index = displayRestaurants.number - 1 
+	max_index = len(paginator.page_range)
+	start_index = index - 2 if index >= 2 else 0
+	end_index = index + 2 if index <= max_index - 2 else max_index
+	print("start_index:",start_index)
+	print("end_index:",end_index)
+	page_range = paginator.page_range[start_index:end_index]
+	return render(request, 'Foodhubinit/listCityRestaurants.html',{"city":city_name,"nearby_cities":nearbyCities,"restaurants":displayRestaurants,"total":totalRestaurants,"ratings":ratings,"page_range":page_range})	
 
 def custom_login(request):
     if request.user.is_authenticated():
         return redirect('home')
     else:
-        return login(request)	
+        return login(request)
+
+
+def getRestaurants(type,place):
+	URL = "https://api.eatstreet.com/publicapi/v1/restaurant/search";
+	URL+= "?method="+type+"&street-address="+urllib.parse.quote(place);
+	print("url here:",URL)
+	req = urllib.request.Request(URL)
+	req.add_header('X-Access-Token', '__API_EXPLORER_AUTH_KEY__')
+	content = urllib.request.urlopen(req)
+	contentReturned = content.read()
+	encoding = content.info().get_content_charset('utf-8')
+	JSON_object = json.loads(contentReturned.decode(encoding))
+	return JSON_object['restaurants'];    
+
+def find_restaurants(request,address):
+	pageNum = request.GET.get('pageNum')
+	orderType = "delivery"
+	ratings =dict();
+	if request.GET.get('orderType'):
+		orderType = request.GET.get('orderType')
+
+	restaurants = getRestaurants(orderType,address)
+	request.session["restaurants"] = restaurants;
+	cuisines = getCuisines(restaurants);
+	print("cuisines here:",cuisines)
+	displayRestaurants,page_range=getRestaurantDetails(restaurants,pageNum)
+	return render(request, 'Foodhubinit/listRestaurants.html',{"restaurants":displayRestaurants,"ratings":ratings,"page_range":page_range,"cuisines":cuisines,"address":json.dumps(address)})	
+
+def getRestaurantDetails(restaurants,pageNum):
+	paginator = Paginator(restaurants, 20)
+	try:
+		displayRestaurants = paginator.page(pageNum)
+	except PageNotAnInteger:
+		displayRestaurants = paginator.page(1)
+	except EmptyPage:
+		displayRestaurants = paginator.page(paginator.num_pages)
+
+	index = displayRestaurants.number - 1 
+	max_index = len(paginator.page_range)
+	start_index = index - 2 if index >= 2 else 0
+	end_index = index + 2 if index <= max_index - 2 else max_index
+	print("start_index:",start_index)
+	print("end_index:",end_index)
+	page_range = paginator.page_range[start_index:end_index]
+	return displayRestaurants,page_range
+
+def getCuisines(restaurants):
+	cuisines =dict()
+	for restaurant in restaurants:
+		for foodType in restaurant['foodTypes']:
+			if foodType.lower() in cuisines:
+				value = cuisines[foodType.lower() ]
+				cuisines[foodType.lower()] = value + 1
+			else:
+				if foodType !='':
+					cuisines[foodType.lower()] =1
+
+	return cuisines
+
+def restaurant_description(request, restaurant_name):
+
+	restaurantDesc =dict()
+	if request.session["restaurants"]:
+		for restaurant in request.session["restaurants"]:
+			if restaurant["name"] == restaurant_name:
+				restaurantDesc = restaurant
+				break
+
+	print("restaurant here:",restaurantDesc)
+	menu= getRestaurantMenu(restaurantDesc['apiKey'])
+	print("menu here:",menu)
+	return render(request, 'Foodhubinit/restaurantPage.html',{"restaurant":restaurantDesc,"menus":menu})
+	pass
+
+def getRestaurantMenu(apiKey):
+	URL = "https://api.eatstreet.com/publicapi/v1/restaurant/"+apiKey+"/menu";
+	URL+= "?includeCustomizations=false";
+	print("url here:",URL)
+	req = urllib.request.Request(URL)
+	req.add_header('X-Access-Token', '__API_EXPLORER_AUTH_KEY__')
+	content = urllib.request.urlopen(req)
+	contentReturned = content.read()
+	encoding = content.info().get_content_charset('utf-8')
+	JSON_object = json.loads(contentReturned.decode(encoding))
+	return JSON_object;
+
+
