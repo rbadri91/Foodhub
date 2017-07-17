844e90510fc841ac896acf2f2ba7f11cd5377ec7
rbadri91 <rbadri01@gmail.com>
2017-07-16 21:06:32 -0400

    Modifying changes

--- Foodhub/settings.py
+++ Foodhub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'b7=r98-9tdurhftav3gea@i%g!zanwlrsido@2#o8i9&^(x1$-'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
